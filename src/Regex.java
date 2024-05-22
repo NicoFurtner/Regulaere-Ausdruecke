079da7d551d87f4bf64d9ca2b646ea8f2cc741bd
Nico Furtner <nfurtner@student.tgm.ac.at>
2024-05-22 15:36:31 +0200

    Konstruktor und Attribute

--- src/Regex.java
+++ src/Regex.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * REgex KLasse von Furtner Nico
+ * @author Nico Furtner
+ * @version 2024-05-22
+ */
 public class Regex {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private List<String> strings;
+    private String regex;
+    public Regex() {
+        strings = new ArrayList<>();
+        regex = "^(?!-)(?!.*--)([a-zA-Z0-9-]+\\.)*[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(?<!-)$|^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$";
     }
 }
